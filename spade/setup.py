dc3fc37a11ff4c53f90c7dea8fc6ff4d21243a9c
davidaraujo06 <ddsaa@isep.ipp.pt>
2024-06-19 11:38:17 +0100

    lkjnb

--- spade/setup.py
+++ spade/setup.py
@@ -1,8 +1,8 @@
 from production_line_agent_1 import LinhaProducao1Agent
 from production_line_agent_2 import LinhaProducao2Agent
 from production_line_agent_3 import LinhaProducao3Agent
-from robot_agent_1 import RobotAgent1
-from robot_agent_2 import RobotAgent2
+from robot_agent_1 import RobotAgent
+#from robot_agent_2 import RobotAgent2
 import asyncio
 
 async def main():    
@@ -10,8 +10,8 @@ async def main():
     linha1_agent = LinhaProducao1Agent("linha1@jabbers.one", "123linha1")
     linha2_agent = LinhaProducao2Agent("linha2@jabbers.one", "123linha2")
     linha3_agent = LinhaProducao3Agent("linha3@jabbers.one", "123linha3")
-    robo1_agent = RobotAgent1("robo1@jabbers.one", "123robo1")
-    robo2_agent = RobotAgent2("robo2@jabbers.one", "123robo2")
+    #robo1_agent = RobotAgent("robo1@jabbers.one", "123robo1")
+    #robo2_agent = RobotAgent2("robo2@jabbers.one", "123robo2")
 
     #await encomenda_agent.start(auto_register=True)
     await linha1_agent.start(auto_register=True)
